74f84cefd067dc8c407dcfbb941957c9ea9b252d
David Madrid <davidmadridpathway@gmail.com>
2022-04-22 15:11:05 -0500

    Hook state fixed

--- src/components/slidebar.js
+++ src/components/slidebar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Slidebar({ handleClick }) {
   return (
-    <div className='text-white space-y-10 text-2xl bg-neutral-800 flex absolute  flex-col bg-blue-300 items-center justify-center h-screen w-screen p-10'>
+    <div className='text-white space-y-10 text-2xl flex absolute flex-col bg-neutral-700 items-center justify-center h-screen w-screen'>
       <button className='transition ease-in-out hover:translate-x-5 p-3' onClick={handleClick}>
         <Link to='/'>Home</Link>
       </button >
