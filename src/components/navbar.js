40ca1f98700f26c3c08f2f798d36faa4e4ea742d
David Madrid <davidmadridpathway@gmail.com>
2022-04-25 16:50:59 -0500

    First review

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,12 +12,12 @@ const Navbar = () => {
       <div className={`md:hidden -translate-x-full ${click ? 'transition duration-500 translate-x-0' : 'transition duration-500'}`}>
         <Slidebar handleClick={handleClick} />
       </div>
-      <nav className={`static md:flex md:justify-between bg-slate-700 text-white `}>
+      <nav className={`md:flex md:justify-between bg-slate-700 text-white `}>
         <div className={`flex justify-between px-10 items-center py-5`}>
           <i>logo</i>
           <button className={`md:hidden absolute top-5 right-5 ${click ? 'transition duration-300 -rotate-90' : 'transition duration-300 rotate-0'}`} onClick={handleClick}><FaBars /></button>
         </div>
-        <div className='hidden md:flex px-10 space-x-10'>
+        <div className='hidden px-10 space-x-10 md:flex'>
           <button className='hover:scale-x-125 transition ease-in-out hover:-translate-y-1'>
             <Link to='/'>Home</Link>
           </button>
--- src/components/slidebar.js
+++ src/components/slidebar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Slidebar({ handleClick }) {
   return (
-    <div className='text-white space-y-10 text-2xl flex absolute flex-col bg-neutral-700 items-center justify-center h-screen w-screen'>
+    <div className='flex flex-col items-center justify-center absolute h-screen w-screen space-y-10 bg-black opacity-80 text-white text-2xl '>
       <button className='transition ease-in-out hover:translate-x-5 p-3' onClick={handleClick}>
         <Link to='/'>Home</Link>
       </button >
@@ -12,6 +12,9 @@ function Slidebar({ handleClick }) {
       </button>
       <button className='transition ease-in-out hover:translate-x-5 p-3' onClick={handleClick}>
         <Link to='/products'>Products</Link>
+      </button>
+      <button className='transition ease-in-out hover:translate-x-5 p-3 rounded-3xl border-2 border-white' onClick={handleClick}>
+        <Link to='/get-started'>Get Started</Link>
       </button>
     </div >
   )
