39996d3a13fc3cc23135e377f0dfd74a416d0f5b
David Madrid <davidmadridpathway@gmail.com>
2022-04-26 12:23:28 -0500

    Adding the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,36 +2,36 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-neutral-800 font-bold text-white '>
-      <h1>Join the Adventure newsletter to recieve our best vacations deals</h1>
-      <h3>You can subscribe at any time</h3>
-      <form action="">
-        <input type="text" />
-        <button>Subscribe</button>
-      </form>
-      <div className='flex space-x-10'>
-        <div>
+    <div className='bg-neutral-800 font-bold text-white border-box py-10'>
+      <h1 className='text-center text-xl p-5'>Join the Adventure newsletter to recieve our best vacations deals</h1>
+      <h3 className='text-center p-5'>You can subscribe at any time</h3>
+      <div className='flex flex-col space-y-3'>
+        <input className='py-2' type="text" placeholder='Enter your email' />
+        <button className='border-2 py-2 text-white font-bold border-white rounded'>Subscribe</button>
+      </div>
+      <div className='flex md:space-x-10 flex-wrap justify-center items-center'>
+        <div className='border-2 border-white p-3 m-3'>
           <h1>About Us</h1>
           <p>How it works</p>
           <p>Testimonials</p>
           <p>Carrer</p>
           <p>Investors</p>
         </div>
-        <div>
+        <div className='border-2 border-white p-3 m-3'>
           <h1>Contact Us</h1>
           <p>Contact</p>
           <p>Support</p>
           <p>Destinations</p>
           <p>Sponsorship</p>
         </div>
-        <div>
+        <div className='border-2 border-white p-3 m-3'>
           <h1>Videos</h1>
           <p>Submit Video</p>
           <p>Ambassadors</p>
           <p>Agency</p>
           <p>Influencer</p>
         </div>
-        <div>
+        <div className='border-2 border-white p-3 m-3'>
           <h1>Social Media</h1>
           <p>Instagram</p>
           <p>Facebook</p>
